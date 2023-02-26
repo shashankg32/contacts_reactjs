ef69ef08dd3a945fe5ae783efb552b689e5f9f67
shashankg32 <shashank6511@gmail.com>
2023-02-26 15:17:06 +0530

    creating .map() function video no 413

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,23 @@ import React from "react";
 import Card from "../components/Card";
 import contacts from "../contacts";
 
+function createCard(contacts)
+{
+    return <Card 
+    key={contacts.id}
+    name={contacts.name}
+    img={contacts.imgURL}
+    tel={contacts.phone}
+    email={contacts.email}
+    />
+}
 function App()
 {
     return (
         <div>
         <h1 className="heading">My Contacts</h1>
-        <Card
+        {contacts.map(createCard)}
+        {/* <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
         tel={contacts[0].phone}
@@ -24,7 +35,7 @@ function App()
         img={contacts[2].imgURL}
         tel={contacts[2].phone}
         email={contacts[2].email}
-        />
+        /> */}
     </div>
     )
 }
